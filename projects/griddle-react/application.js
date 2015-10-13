7c8fef35f909dbaa59e5f845034ad1a8ae76d349
Darshan Gencarelle <darshan@gencarelle.com>
2015-10-12 21:54:13 -0400

    updated griddle table

--- projects/griddle-react/application.js
+++ projects/griddle-react/application.js
@@ -5,39 +5,20 @@ import getJSON from './lib/getJSON'
 
 require('./css/griddle.css')
 var API_URL = 'api/tabledata.js';
-var colMetadata =  [{
-        "columnName": "feed_name",
-        "order": 1,
-        "locked": false,
-        "visible": true,
-        "displayName": "Feed Name"
-    },
-    {
-        "columnName": "feed_type",
-        "order": 2,
-        "locked": false,
-        "visible": true,
-        "displayName": "Feed Type",
-        "customComponent": feedtypeComponent
-        },
-    {
-        "columnName": "geo_locations",
-        "order": 3,
-        "locked": false,
-        "visible": true,
-        "displayName": "Geo Locations"
-        },
-    {
-        "columnName": "topics",
-        "order": 6,
-        "locked": false,
-        "visible": true,
-        "displayName": "Topics"
-        }];
+var divTwitterStyle = {
+    height: '25px',
+    width: '25px',
+
+};
+var divRSSStyle = {
+    height: '25px',
+    width: '25px',
+
+};
 
 var feedtypeComponent = React.createClass({
     render: function() {
-        var feed_type = this.props.data['feed_type']
+        var feed_type = this.props.rowData['feed_type']
         if (typeof feed_type != 'undefined') {
             switch (feed_type) {
                 case "twitter":
@@ -48,6 +29,7 @@ var feedtypeComponent = React.createClass({
                     break;
             }
         }
+        return (<div></div>);
     }
 });
 
@@ -74,13 +56,47 @@ const App = React.createClass({
     },
 
     render() {
+        var colMetadata =  [{
+            'columnName': 'feed_name',
+            'order': 1,
+            'locked': true,
+            'visible': true,
+            'displayName': 'Feed Name'
+        },
+            {
+                'columnName': 'feed_type',
+                'order': 2,
+                'locked': true,
+                'visible': true,
+                'displayName': 'Feed Type',
+                'customComponent': feedtypeComponent,
+                'cssClassName': 'cellCenter'
+            },
+            {
+                'columnName': 'geo_locations',
+                'order': 3,
+                'locked': true,
+                'visible': true,
+                'displayName': 'Geo Locations',
+                'cssClassName': 'cellCenter'
+            },
+            {
+                'columnName': 'topics',
+                'order': 6,
+                'locked': true,
+                'visible': true,
+                'displayName': 'Topics',
+                'cssClassName': 'cellCenter'
+            }
+        ];
+
         return <div>
             <Griddle
                 columnMetadata={colMetadata}
                 tableClassName={{textAlign: "center"}}
                 results={this.state.data}
                 tableClassName="Feeds"
-                showSettings={true}
+                showSettings={false}
                 columns={["feed_name", "feed_type", "geo_locations", "topics"]}
                 onRowClick={this._rowClick}
                 />
